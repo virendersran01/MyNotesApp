17cd8875baeb829d5fd602f80675861dc4f147ec
virendersran01 <virendersran01@gmail.com>
2020-06-15 17:44:55 +0530

    Created Room database.

---
+++ app/src/main/java/com/virtualstudios/notesapp/activities/CreateNoteActivity.java
@@ -0,0 +1,98 @@
+package com.virtualstudios.notesapp.activities;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.virtualstudios.notesapp.R;
+import com.virtualstudios.notesapp.database.NotesDatabase;
+import com.virtualstudios.notesapp.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    private EditText inputNoteTitle, inputNoteSubTitle, inputNoteText;
+    private TextView textDateTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubTitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNote);
+        textDateTime = findViewById(R.id.textDateTime);
+
+        textDateTime.setText(
+                new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                        .format(new Date())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+    }
+
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (inputNoteSubTitle.getText().toString().trim().isEmpty()
+                && inputNoteText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubTitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDateTime(textDateTime.getText().toString());
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void>{
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NotesDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+
+        new SaveNoteTask().execute();
+    }
+}
---
+++ app/src/main/java/com/virtualstudios/notesapp/activities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.virtualstudios.notesapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.virtualstudios.notesapp.R;
+import com.virtualstudios.notesapp.database.NotesDatabase;
+import com.virtualstudios.notesapp.entities.Note;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
+
+        getNotes();
+    }
+
+    private void getNotes(){
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>>{
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+        }
+
+        new GetNotesTask().execute();
+    }
+}
